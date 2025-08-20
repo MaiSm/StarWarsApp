c6ed402cce24f5631d24ed8651544d33a422ade1
Mailen <mailen.salasmir@gmail.com>
2025-08-20 20:02:19 -0300

    edit basic config

---
+++ src/main/java/com/swappi/SwapiApplication.java
@@ -0,0 +1,14 @@
+package com.swappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SwapiApplication.class, args);
+        System.out.printf("Starting Swapi App");
+
+    }
+}
